36140505771cfe5e9ea27843ae7d9154fa150b38
Anna Peloušková <ann.pelouskova@gmail.com>
2023-10-03 20:06:04 +0200

    Preformatovani podle doporuceni

--- main.py
+++ main.py
@@ -11,39 +11,45 @@ from volby_cz_tools import *
 import csv
 import sys
 
+
 def combine_results(counter, increase):
-    if (counter == {}):
+    if counter == {}:
         return increase
     for key in counter.keys():
-        if (not key.isdigit() and counter[key].isdigit()):
+        if not key.isdigit() and counter[key].isdigit():
             counter[key] = str(int(counter[key]) + int(increase[key]))
         else:
-            counter[key]["hlasy"] = str(int(counter[key]["hlasy"]) + 
-                                        int(increase[key]["hlasy"]))
+            counter[key]["hlasy"] = str(
+                int(counter[key]["hlasy"]) + int(increase[key]["hlasy"])
+            )
     return counter
 
+
 def generate_csv_header(obec_details, election_results):
-    header = ['code', 'location', 'registered', 'envelopes', 'valid']
+    header = ["code", "location", "registered", "envelopes", "valid"]
     for key in election_results.keys():
-        if (key.isdigit()):
+        if key.isdigit():
             header = header + [election_results[key]["jmeno"]]
-    
+
     return header
 
+
 def generate_csv_row(obec_details, election_results):
-    row = [obec_details["code"], 
-           obec_details["location"], 
-           election_results["registered"], 
-           election_results["envelopes"], 
-           election_results["valid"]]
+    row = [
+        obec_details["code"],
+        obec_details["location"],
+        election_results["registered"],
+        election_results["envelopes"],
+        election_results["valid"],
+    ]
     for key in election_results.keys():
-        if (key.isdigit()):
+        if key.isdigit():
             row = row + [election_results[key]["hlasy"]]
 
     return row
 
-def main():
 
+def main():
     if len(sys.argv) != 3:
         print("Zadej dva argumenty.")
         exit()
@@ -55,14 +61,14 @@ def main():
 
     try:
         parsed_address = urlparse(scraped_address)
-        if (parsed_address.scheme == ''):
+        if parsed_address.scheme == "":
             raise URLError("Adresa neni validni")
         urlopen(scraped_address)
     except URLError:
         print(scraped_address, "neni validni adresa.")
-        exit()  
+        exit()
 
-    f = open(export_filename, 'w', encoding='utf-8')
+    f = open(export_filename, "w", encoding="utf-8")
     writer = csv.writer(f)
     header_written = False
     obce = []
@@ -79,37 +85,39 @@ def main():
         election_row["code"] = obec["cislo"]
         election_row["location"] = obec["nazev"]
 
-        r = requests.get(requests.compat.urljoin(scraped_address, 
-                                                obec["odkaz"]))
+        r = requests.get(requests.compat.urljoin(scraped_address, obec["odkaz"]))
         html = r.content
         okrsek = is_okrsek_list_page(html)
-        if (okrsek):
+        if okrsek:
             okrsky = parse_okrsek_list_page(html)
             election_results_counter = {}
             for okrsky_item in okrsky:
-                r = requests.get(requests.compat.urljoin(scraped_address, 
-                                okrsky_item["odkaz"]))
+                r = requests.get(
+                    requests.compat.urljoin(scraped_address, okrsky_item["odkaz"])
+                )
                 html = r.content
                 election_results = parse_detail_page(html)
                 election_results_counter = combine_results(
-                    election_results_counter, 
-                    election_results
-                    )
+                    election_results_counter, election_results
+                )
             election_results = election_results_counter
         else:
             election_results = parse_detail_page(html)
 
-        if (not header_written):
+        if not header_written:
             header = generate_csv_header(election_row, election_results)
-            if verbose: print(header)
+            if verbose:
+                print(header)
             writer.writerow(header)
             header_written = True
-        
+
         row = generate_csv_row(election_row, election_results)
         writer.writerow(row)
-        if verbose: print(election_row, election_results)
+        if verbose:
+            print(election_row, election_results)
 
     f.close
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- volby_cz_tools.py
+++ volby_cz_tools.py
@@ -7,45 +7,48 @@ discord: Pel#9400
 
 from bs4 import BeautifulSoup
 
+
 def is_okrsek_list_page(html):
     parsed_html = BeautifulSoup(html, features="html.parser")
-    table = parsed_html.body.find('th', attrs={'id':'s1'})
-    if (table is None):
+    table = parsed_html.body.find("th", attrs={"id": "s1"})
+    if table is None:
         return False
-    if (table.text != "Okrsek"):
+    if table.text != "Okrsek":
         return False
     return True
 
+
 def is_detail_page(html):
     parsed_html = BeautifulSoup(html, features="html.parser")
-    scraped_registered = parsed_html.body.find('td', attrs={'headers':'sa2'})
+    scraped_registered = parsed_html.body.find("td", attrs={"headers": "sa2"})
     if scraped_registered:
         return True
     return False
 
+
 def parse_detail_page(html):
     result_set = {}
     parsed_html = BeautifulSoup(html, features="html.parser")
-    scraped_registered = parsed_html.body.find('td', attrs={'headers':'sa2'})
+    scraped_registered = parsed_html.body.find("td", attrs={"headers": "sa2"})
     if scraped_registered:
         result_set["registered"] = scraped_registered.text.replace("\xa0", "")
-    scraped_envelopes = parsed_html.body.find('td', attrs={'headers':'sa3'})
+    scraped_envelopes = parsed_html.body.find("td", attrs={"headers": "sa3"})
     if scraped_envelopes:
         result_set["envelopes"] = scraped_envelopes.text.replace("\xa0", "")
-    scraped_valid = parsed_html.body.find('td', attrs={'headers':'sa6'})
+    scraped_valid = parsed_html.body.find("td", attrs={"headers": "sa6"})
     if scraped_valid:
         result_set["valid"] = scraped_valid.text.replace("\xa0", "")
 
     tables = parsed_html.body.find_all("table", {"class": "table"})
-    
+
     election_results = tables[1].find_all("tr")
     for party in election_results:
         votes = {}
-        cislo = party.find('td', attrs={'class':'cislo', 'headers': "t1sa1 t1sb1"})
-        if (not cislo):
+        cislo = party.find("td", attrs={"class": "cislo", "headers": "t1sa1 t1sb1"})
+        if not cislo:
             continue
-        jmeno = party.find('td', attrs={'class':'overflow_name'})
-        num_votes = party.find('td', attrs={'class':'cislo', 'headers': "t1sa2 t1sb3"})
+        jmeno = party.find("td", attrs={"class": "overflow_name"})
+        num_votes = party.find("td", attrs={"class": "cislo", "headers": "t1sa2 t1sb3"})
         votes["jmeno"] = jmeno.text
         votes["hlasy"] = num_votes.text
         result_set[cislo.text] = votes
@@ -53,47 +56,49 @@ def parse_detail_page(html):
     election_results = tables[2].find_all("tr")
     for party in election_results:
         votes = {}
-        cislo = party.find('td', attrs={'class':'cislo', 'headers': "t2sa1 t2sb1"})
-        if (not cislo):
+        cislo = party.find("td", attrs={"class": "cislo", "headers": "t2sa1 t2sb1"})
+        if not cislo:
             continue
-        jmeno = party.find('td', attrs={'class':'overflow_name'})
-        num_votes = party.find('td', attrs={'class':'cislo', 'headers': "t2sa2 t2sb3"})
+        jmeno = party.find("td", attrs={"class": "overflow_name"})
+        num_votes = party.find("td", attrs={"class": "cislo", "headers": "t2sa2 t2sb3"})
         votes["jmeno"] = jmeno.text
         votes["hlasy"] = num_votes.text
         result_set[cislo.text] = votes
 
     return result_set
 
+
 def parse_list_page(html):
     result_set = []
     parsed_html = BeautifulSoup(html, features="html.parser")
 
-    multiple_tables = parsed_html.body.findAll('table', attrs={'class':'table'})
+    multiple_tables = parsed_html.body.findAll("table", attrs={"class": "table"})
     for table in multiple_tables:
-        parsed_obce = table.findAll('tr')
+        parsed_obce = table.findAll("tr")
         for parsed_obec in parsed_obce:
-            cislo = parsed_obec.find('td', attrs={'class':'cislo'})
-            nazev_obce = parsed_obec.find('td', attrs={'class':'overflow_name'})
-            detail = parsed_obec.find('td', attrs={'class':'center'})
+            cislo = parsed_obec.find("td", attrs={"class": "cislo"})
+            nazev_obce = parsed_obec.find("td", attrs={"class": "overflow_name"})
+            detail = parsed_obec.find("td", attrs={"class": "center"})
             if detail:
                 obec = {}
                 obec["nazev"] = nazev_obce.text
                 obec["cislo"] = cislo.text
-                obec["odkaz"] = detail.a['href']
+                obec["odkaz"] = detail.a["href"]
                 result_set.append(obec)
 
     return result_set
+
 
 def parse_okrsek_list_page(html):
     result_set = []
     parsed_html = BeautifulSoup(html, features="html.parser")
 
-    table = parsed_html.body.find('table', attrs={'class':'table'})
-    details = table.findAll('td', attrs={'class':'cislo'})
-    
+    table = parsed_html.body.find("table", attrs={"class": "table"})
+    details = table.findAll("td", attrs={"class": "cislo"})
+
     for detail in details:
         obec = {}
-        obec["odkaz"] = detail.a['href']
+        obec["odkaz"] = detail.a["href"]
         result_set.append(obec)
 
     return result_set
